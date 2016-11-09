85de22f20d72f15f891755106a345da6ca6551b7
FangLin001 <823554146@qq.com>
2016-11-09 18:40:31 +0800

    task1
    本地数据库的搭建

---
+++ e-Healthy/e-Healthy/Model/FLPhoneLoginModel.m
@@ -0,0 +1,13 @@
+//
+//  FLPhoneLoginModel.m
+//  e-Healthy
+//
+//  Created by FangLin on 16/11/9.
+//  Copyright © 2016年 FangLin. All rights reserved.
+//
+
+#import "FLPhoneLoginModel.h"
+
+@implementation FLPhoneLoginModel
+
+@end
---
+++ e-Healthy/e-Healthy/Utils/PhoneLoginUtils.h
@@ -0,0 +1,17 @@
+//
+//  PhoneLoginUtils.h
+//  e-Healthy
+//
+//  Created by FangLin on 16/11/9.
+//  Copyright © 2016年 FangLin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class FLPhoneLoginModel;
+
+@interface PhoneLoginUtils : NSObject
+
++ (void)phoneLoginData:(NSDictionary *)dict;
+
+@end
