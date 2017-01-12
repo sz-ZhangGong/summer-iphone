b5dd43136442ff208e8d4a4d6d537412282eb794
FangLin001 <823554146@qq.com>
2017-01-12 21:01:09 +0800

    修改bundle id

--- summer/MyConfig.h
+++ summer/MyConfig.h
@@ -10,7 +10,7 @@
 #define MyConfig_h
 
 
-#define URL_APP_ROOT @"http://testehealth.zhongyinginfo.com"//域名地址
+#define URL_APP_ROOT @"http://testehealth.zhongyinginfo.com"//域名地址//预发布版
 
 #define WELCOME_IMAGES_COUNT 4//欢迎页图片数量
 
