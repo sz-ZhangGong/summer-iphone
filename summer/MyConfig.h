f3903d3a6960e23dc97313e3428195503ba10561
FangLin001 <823554146@qq.com>
2017-01-12 21:05:13 +0800

    更改bundle id

--- summer/MyConfig.h
+++ summer/MyConfig.h
@@ -10,7 +10,7 @@
 #define MyConfig_h
 
 
-#define URL_APP_ROOT @"http://ehealth.lucland.com"//域名地址
+#define URL_APP_ROOT @"http://ehealth.lucland.com"//域名地址//内测版
 
 #define WELCOME_IMAGES_COUNT 4//欢迎页图片数量
 
