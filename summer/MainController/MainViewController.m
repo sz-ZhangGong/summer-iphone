94e6288c2b313ef95d32a1c79c161978536d1fb6
FangLin001 <823554146@qq.com>
2017-01-16 20:23:46 +0800

    优化扫描健康卡卡号功能

--- summer/MainController/MainViewController.m
+++ summer/MainController/MainViewController.m
@@ -142,7 +142,8 @@ static NSString *const mainUrlStr = @"/forms/FrmIndex,/forms/Login,/forms/Verifi
     self.view.backgroundColor = [UIColor whiteColor];
     //出错页面
     [self.webView addSubview:self.errorImageView];
-    
+    //webview
+    [self.view addSubview:self.webView];
     //右边按钮下拉菜单
     [self settingMenu];
     
@@ -152,8 +153,6 @@ static NSString *const mainUrlStr = @"/forms/FrmIndex,/forms/Login,/forms/Verifi
 {
     [super viewWillAppear:animated];
 //    [YMWebCacheProtocol start];
-    //webview
-    [self.view addSubview:self.webView];
     //网络监听
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(getLoadDataBase:) name:KLoadDataBase object:nil];
     //监听支付成功
